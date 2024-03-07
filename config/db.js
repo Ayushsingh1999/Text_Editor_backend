84d1ffa18192134479fdc9fe3f80f1da85d4acff
Ayushsingh1999 <ayushsigh842018@gmail.com>
2024-03-07 19:03:58 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const dotenv =  require('dotenv')
+dotenv.config() 
+
+const mongoose = require('mongoose')
+
+
+mongoose.connect(process.env.mongourl).then(()=>
+    {
+        console.log("Database Connected :)")
+    }).catch((err)=>
+    {
+        console.log("this is error :(",err)
+    })
